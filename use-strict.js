5ff6f4b195addb8e321ce36e539fd9c99d2c6165
Swellmary <93521886+Swellmary@users.noreply.github.com>
2025-06-19 16:08:24 +0800

    Add files via upload

---
+++ use-strict.js
@@ -0,0 +1,7 @@
+
+
+"use strict"; // this will make sure the codes to run in modern version
+
+height = 190; //old version of codes without declaration of variable - this works without the "use strict" declaration
+
+console.log(height);
---
+++ variableValues.js
@@ -0,0 +1,13 @@
+let greetings = "hello";
+console.log(greetings);
+
+greetings = 23;
+console.log(greetings)
+
+
+console.log(greetings);
+console.log(greetings);
+console.log(greetings);
+console.log(greetings);
+console.log(greetings); 
+console.log(greetings);
