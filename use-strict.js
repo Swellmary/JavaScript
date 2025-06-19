5a886ebd5af1137fb1b7e520261f57adf058b199
Swellmary <93521886+Swellmary@users.noreply.github.com>
2025-06-19 16:09:53 +0800

    Delete use-strict.js

--- use-strict.js
+++
@@ -1,7 +0,0 @@
-
-
-"use strict"; // this will make sure the codes to run in modern version
-
-height = 190; //old version of codes without declaration of variable - this works without the "use strict" declaration
-
-console.log(height);
