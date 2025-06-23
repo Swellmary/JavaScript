3749cd0aef5f25ca2739232f829b2534f5753b97
Swellmary <suarezwellmary@gmail.com>
2025-06-23 19:34:13 +0800

    Section 3 [Object];

---
+++ JavaScript-101/JSE Module 2 - Variables, Data Type, Type Casting, Comments/Section 3/object.js
@@ -0,0 +1,21 @@
+let object = {};
+console.log(typeof object);
+
+
+let wholeObject = {
+    str: "hi, I'm ",
+    num: 34
+
+    // console.log(str + num); ❌ Invalid but you can create a function and there create console method
+};
+console.log(wholeObject);
+console.log(wholeObject.str); // getting the properties only
+console.log(wholeObject.str + wholeObject.num);
+
+wholeObject.str = "hey! I'm ";
+wholeObject.num = 20 + String(" something");
+console.log(wholeObject.str + wholeObject.num); //hey! I'm 20 something
+
+delete wholeObject.num;
+
+console.log(wholeObject.num) //undefined
