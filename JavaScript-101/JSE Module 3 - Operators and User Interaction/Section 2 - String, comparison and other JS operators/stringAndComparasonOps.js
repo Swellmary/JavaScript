0e4e9a4e46a52aaf80915e14af8df87b1ee3c624
Swellmary <suarezwellmary@gmail.com>
2025-06-24 21:47:25 +0800

    Module 3 Section 3: String and Comparison Operators

---
+++ JavaScript-101/JSE Module 3 - Operators and User Interaction/Section 2 - String, comparison and other JS operators/stringAndComparasonOps.js
@@ -0,0 +1,14 @@
+// LOGICAL OPERATORS
+
+
+console.log(10 === "10"); //false
+console.log(10 == "10"); // true
+
+console.log(10 === 10); // true
+console.log(10 == 10); // true
+
+console.log(10 !== "10"); //true, the === turns to == when using !(not)
+console.log(10 != "10"); // false, the == turns to = when using !(not)
+
+console.log(10 !== 10); // false
+console.log(10 != 10); // false
