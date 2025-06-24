e417f5e13cb4418a53b0cfd92ec94658ebcf2e97
Swellmary <suarezwellmary@gmail.com>
2025-06-24 22:30:17 +0800

    Module 3 Section 1: Logical Operator

--- JavaScript-101/JSE Module 3 - Operators and User Interaction/Section 1 - Assignment, arithmetic, and logical operators/logicalOperators.js
+++ JavaScript-101/JSE Module 3 - Operators and User Interaction/Section 1 - Assignment, arithmetic, and logical operators/logicalOperators.js
@@ -1,3 +1,50 @@
-// LOGICAL OPERATORS
 
+// LOGICAL OPERATORS:  &&:AND, ||:OR, !:NOT
 
+console.log("AND");
+
+// && - AND
+console.log(true && true); // -> true
+console.log(true && false); // -> false
+console.log(false && true); // -> false
+console.log(false && false); // -> false, since neither is true the whole thing is false.
+
+console.log();
+console.log("OR");
+
+// || - OR 
+console.log(true || true); // -> true
+console.log(true || false); // -> true
+console.log(false || true); // -> true
+console.log(false || false); // -> false, since neither is true the whole thing is false.
+
+console.log();
+console.log("OR");
+
+// ! - NOT
+console.log(!true); // -> false
+console.log(!false); // -> true
+
+const a = false;
+const b = true;
+const c = false;
+const d = true;
+
+console.log();
+console.log("overall example: ");
+
+// !, ||, && 
+console.log(a && b && c || d); // -> true
+console.log(a && b && (c || d)); // -> false
+
+/*ACTUAL LOGIC: 
+
+a && b = false && true = false
+false && c = false && false = false
+false || d = false || true = ✅ true
+
+(c || d) = false || true = true
+a && b = false && true = false
+false && true = ✅ false
+
+*/
