7699d30afc3593142986491cc5ba8be4b6a26900
Swellmary <suarezwellmary@gmail.com>
2025-06-24 21:16:12 +0800

    Module 3 Section 1: Assignment, Arithmetic, Unary Arithmetic, Unary Decrement and Increment Operators

---
+++ JavaScript-101/JSE Module 3 - Operators and User Interaction/assignmentArithmetic.js
@@ -0,0 +1,64 @@
+
+
+//ASSIGNMENT (=)
+
+let year = 2050;
+let nextYear = year = 2051; // -> this means that we declare a next year and we also changed the value of the year inside the next year initialization/declaration
+
+console.log(`this year: ${year}`); // 2051
+console.log(`next year: ${nextYear}`); // 2051
+console.log("");
+
+//ARITHMETHIC (+, -, *, /, %, **)
+
+const x = 2;
+const y = 6;
+
+console.log("addition:", 2 + 6);
+console.log("minus:", 2 - 6);
+console.log("division:", 2 / 6);
+console.log("multiplication:", 2 * 6);
+console.log("division remainder:", 2 % 6);
+console.log("exponent:", 2 ** 6);
+
+//UNARY ARITHMETIC OPERATORS
+
+let str = "123";
+let n1 = +str;
+let n2 = -str;
+let n3 = -n2;
+let n4 = +"abcd"; 
+
+console.log(`${str} : ${typeof str}`); //123
+console.log(`${n1} : ${typeof n1}`); //123
+console.log(`${n2} : ${typeof n2}`); //-123
+console.log(`${n3} : ${typeof n3}`); //123
+console.log(`${n4} : ${typeof n4}`); //NaN
+
+//UNARY INCREMENT AND DECREMENT
+
+let n = 5;
+
+console.log(n++); // -> 6
+console.log(n); // -> 7 
+
+let z = 3;
+
+console.log(++z); // -> 4
+console.log(z++); // -> 4 returns the original value
+console.log(z); // -> 5 // then returns the value coming from n++, which affects only after one declaration
+
+//the line of code was interpreted like this 
+
+{
+    // ++n
+    n = n + 1;
+
+    //n++
+    let temp = z;
+    z = z + 1;
+    return temp;
+};
+
+
+
---
+++ JavaScript-101/JSE Module 3 - Operators and User Interaction/incrementDecrement.js
@@ -0,0 +1,14 @@
+
+
+// URINARY INCREMENT AND DECREMENT OPERATORS
+
+
+
+
+
+
+
+
+
+
+
