eda29039f6c0b41d656a1dd01f57c1cf69acfa2a
Swellmary <suarezwellmary@gmail.com>
2025-06-30 22:18:34 +0800

    module 4 if else

--- JavaScript-101/JSE Module 4 - Conditional execution/if-else-statement.js
+++ JavaScript-101/JSE Module 4 - Conditional execution/if-else-statement.js
@@ -1,9 +1,13 @@
 
 // if else
 
+let isUserReady = confirm("Are you ready!?");
 
-
-
+if (isUserReady) {
+    console.log("user is ready!");
+} else {
+    console.log("user is not ready!");
+}
 
 
 
